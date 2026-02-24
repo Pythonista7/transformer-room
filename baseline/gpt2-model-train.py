78dbeaddd0dba19b8b0bc05ee488b6d5cea0b729
Ash <36104244+Pythonista7@users.noreply.github.com>
2026-02-24 16:09:36 +0530

    refactor: sep train from config runs

---
+++ baseline/gpt2-model-train.py
@@ -0,0 +1,33 @@
+from config import Config
+from train import model_pipeline
+
+if __name__ == "__main__":
+    
+    config: Config = {
+        "base_vocab_size": 10_000,
+        "num_special_tokens": 2, # EOS and PAD
+        "vocab_size": 10_002,
+        "d_model": 128,
+        "n_heads": 8,
+        "layers": 2,
+        "learning_rate": 0.001,
+        "epochs": 3,
+        "training_seq_len": 128,
+        "training_stride": 128,
+        "data_fraction": 1,
+        "batch_size": 256,
+        "checkpoint_every_n_steps": 250,
+        "checkpoint_path": "baseline_checkpoint.pt",
+        "final_model_path": "baseline_model.pt",
+        "dataset_path": "../datasets/tiny_shakespeare.txt",
+        "tokenizer_vocab_path": "tiny_shakespeare_bpe_vocab.txt",
+        "resume_from_checkpoint": True,
+        "use_torch_compile": True,
+        "torch_compile_mode": "default",
+        "torch_compile_fullgraph": False,
+        "torch_compile_dynamic": False,
+    }
+    model_pipeline(
+        config,
+        project_name="transformer-room-baseline",
+    )
